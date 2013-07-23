ecf158fb19a57a69cc6673906ca2e3f39572ea91
Jesse Roy James <malion1988@gmail.com>
2013-07-23 15:02:20 -0600

    updates for debug

--- www/js/Main.js
+++ www/js/Main.js
@@ -7,9 +7,19 @@ $(document).ready(function() {
 				cache: false,
 				url: "https://api.instagram.com/v1/media/popular?client_id=1003d9b3eaf04af8b8a649fd90936a32",
 				success: function(data) {
-					for (var i=0; i<10; i++) {
-						$('#photos').append("<a target='_blank' href='" + data.data[i].link +"'><img style='padding:5px' src='" + data.data[i].images.thumbnail.url +"' /></a>");   
+					html = '';
+					for (var i=0; i<1; i++) {
+						html += "<fieldset class='ui-grid-a'><div class='ui-block-a'><a target='_blank' href='" + data.data[i].link +"'><img style='padding:5px' src='" + data.data[i].images.thumbnail.url +"' /></a></div><div class='ui-block-b'><ol>";
+						for(var n=0; n<10; n++){
+							var picture = data.data[i].comments.data[n].from.profile_picture;
+							var name = data.data[i].comments.data[n].from.full_name;
+							console.log(JSON.stringify(picture))
+							console.log(JSON.stringify(name))
+							html += "<li><fieldset class='ui-grid-a'><div class='ui-block-a'><img src='" + picture + "' /><br />" + name + "</div><div class='ui-block-b'><p>" + data.data[i].comments[1][n].text + "</p></div></fieldset></li>";
+						}
+						html += '</ol></div></fieldset>';
 					}
+					$('#photos').html(html)
 				}
 			});
 		});
