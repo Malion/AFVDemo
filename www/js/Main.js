7cb020ff96af21959893a4166245c707a4ff8415
Jesse Roy James <malion1988@gmail.com>
2013-07-23 15:10:12 -0600

    updates

--- www/js/Main.js
+++ www/js/Main.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-	$("#instagram").on('pageinit',function(){
-		$(function() {
-			$.ajax({
-				type: "GET",
-				dataType: "jsonp",
-				cache: false,
-				url: "https://api.instagram.com/v1/media/popular?client_id=1003d9b3eaf04af8b8a649fd90936a32",
-				success: function(data) {
-					for (var i=0; i<10; i++) {
-						$('#photos').append("<a target='_blank' href='" + data.data[i].link +"'><img style='padding:5px' src='" + data.data[i].images.thumbnail.url +"' /></a>");   
-					}
-				}
-			});
-		});
-		$('#instaRefresh').on('click', function(){window.location.reload(true)})
-	});
-	$("#flickr").on('pageinit', function(){
-		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{
-			tagmode: "any",
-			format: "json"
-	  	},
-	  	function(data) {
-			for(var i=0; i<10; i++){
-				$('#flickrPhotos').append("<a target='_blank' href='" + data.items[i].link +"'><img style='padding:5px' src='" + data.items[i].media.m +"' /></a>");
-			}
-	    });
-		$('#flickrRefresh').on('click', function(){window.location.reload(true)})
-	})
-=======
 $(document).ready(function() {
 	$("#instagram").on('pageinit',function(){
 		$(function() {
@@ -68,5 +37,4 @@ $(document).ready(function() {
 	    });
 		$('#flickrRefresh').on('click', function(){window.location.reload(true)})
 	})
->>>>>>> master
 });
